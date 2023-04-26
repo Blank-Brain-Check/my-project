78cbea9741d1f3c9db80053f66d46e5152e390f7
Blank-Brain-Check~ <toshithshetty2@gmail.com>
2023-04-27 00:01:36 +0530

    fixed another bug

--- pages/login.tsx
+++ pages/login.tsx
@@ -32,7 +32,7 @@ function Login() {
   return (
     <div className="relative flex h-screen w-screen flex-col bg-black md:items-center md:justify-center md:bg-transparent">
       <Head>
-        <title>Movie Catalogue</title>
+        <title>Movie_Catalogue</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Image
@@ -40,14 +40,15 @@ function Login() {
         fill={true}
         className="-z-10 !hidden opacity-60 sm:!inline"
         style={{ objectFit: 'cover' }}
-        alt="Bg login page"
+        alt="Bg login page "
       />
-      /* For app logo */
+      
       <img
         src="https://rb.gy/ulxxee"
         className="absolute left-4 top-4 cursor-pointer object-contain md:left-10 md:top-6"
         width={150}
         height={150}
+        alt="App Logo"
       />
 
       <form
